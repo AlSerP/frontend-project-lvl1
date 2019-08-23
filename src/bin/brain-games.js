640cc879cbda81c2fd10bd051dbd778fb85720f0
AlSerP <gosha1416@yandex.ru>
2019-08-23 16:47:00 +0300

    name ask

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
-
 console.log('Welcome to the Brain Games!');
+const user = require('..')
+console.log('Hello, ' + user.name + '!');
---
+++ src/index.js
@@ -0,0 +1,3 @@
+//import {readlineSync} from 'readline-sync';
+var readlineSync = require('readline-sync');
+exports.name = readlineSync.question('May I have your name? ');
