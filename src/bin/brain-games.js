0983e630f2d6574f63debfa05a87e4b7f3411985
AlSerP <gosha1416@yandex.ru>
2019-08-24 19:33:37 +0300

    eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
-const user = require('..')
-console.log('Hello, ' + user.name + '!');
+console.log("Welcome to the Brain Games!");
+const user = require("..");
+console.log("Hello, " + user.name + "!");
--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-//import {readlineSync} from 'readline-sync';
-var readlineSync = require('readline-sync');
-exports.name = readlineSync.question('May I have your name? ');
+var readlineSync = require("readline-sync");
+exports.name = readlineSync.question("May I have your name? ");
